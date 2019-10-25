4d1a8573141011ecb68c9ee38732a7d8bb8c33f4
DentVega <dentvega6@gmail.com>
2019-10-25 00:45:02 -0400

    Initial Commit

---
+++ TourmalineServiceExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TourmalineServiceExample
+//
+//  Created by Brian Dennis Vega Hidalgo on 25-10-19.
+//  Copyright © 2019 Brian Dennis Vega Hidalgo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
