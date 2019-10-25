93661ffd1435b733d5aaa4adbe048c53b4fa42b6
DentVega <dentvega6@gmail.com>
2019-10-25 06:50:21 -0400

    - add Readme File

---
+++ README.md
@@ -0,0 +1,3 @@
+ Tourmaline Service Example in Ios
+
+ This is a test project in Ios of the Tourmaline Labs API.
--- TourmalineServiceExample/ContentView.swift
+++ TourmalineServiceExample/ContentView.swift
@@ -10,8 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List {
+                Section {
+                    Button(action: startService) {
+                        Text("Start Service")
+                    }
+                }
+                Section {
+                    Button(action: startServiceInBackground) {
+                        Text("start service in background")
+                    }
+                }
+            }
+            .navigationBarTitle(Text("Tourmaline Service Example"))
+        }
     }
+}
+
+func startService() {
+    
+}
+
+func startServiceInBackground() {
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
